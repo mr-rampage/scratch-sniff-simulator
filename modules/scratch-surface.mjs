c6ba2fc63a2618fddca1f866c017992564773585
Nelson Lee <nlee@clearpointneuro.com>
2022-10-15 15:48:32 -0400

    moved vibrartion

--- modules/scratch-surface.mjs
+++ modules/scratch-surface.mjs
@@ -25,7 +25,7 @@ function setupView(element) {
 }
     
 function setupEvents(element) {
-    element.addEventListener('pointermove', handleMouseMove(element), navigator.vibrate(200))
+    element.addEventListener('pointermove', handleMouseMove(element));
 }
 
 function createScratchUpEvent() {
