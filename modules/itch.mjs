57c16c109a4f2fc911d190958366bcc87a9f1fac
Nelson Lee <nlee@clearpointneuro.com>
2022-10-15 12:23:37 -0400

    added itch

---
+++ modules/itch.mjs
@@ -0,0 +1,26 @@
+﻿
+// itch is an element
+export function start(itch)
+{
+    const resistance = itch.resistance;
+    const scrathes = 0;
+    itch.addEventListener("scratch-up", () => {})
+    itch.addEventListener("scratch-down", () => {})
+}
+
+function createSatisfied() {
+    return new CustomEvent("itch-satisfied", {
+        bubbles: true,
+        composed: true,
+        cancelable: false
+    });
+}
+
+function getSatisfaction(itch) {
+    switch (itch.resistance) {
+        case "low" : return 3;
+        case "medium" : return 6;
+        case "high" : return 10;
+    }
+    
+}
