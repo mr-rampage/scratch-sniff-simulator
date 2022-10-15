013a845f028ca1cad6d0f76bd1672d9bebc7f2e9
Nelson Lee <nlee@clearpointneuro.com>
2022-10-15 12:16:06 -0400

    added scratch smell

--- modules/app.mjs
+++ modules/app.mjs
@@ -1,3 +1,5 @@
 ﻿import ScratchSurface from "./scratch-surface.mjs";
+import ScratchSmell from "./scratch-smell.mjs";
 
 customElements.define("scratch-surface", ScratchSurface);
+customElements.define("scratch-smell", ScratchSmell, { extends: "span"});
---
+++ modules/scratch-smell.mjs
@@ -0,0 +1,6 @@
+﻿export default class ScratchSmell extends HTMLSpanElement
+{
+    constructor() {
+        super();
+    }
+}
