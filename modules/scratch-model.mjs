1ec7016192a7e3e780ce437c659d1ac5db0b87de
Yousef Sawires <sawires94@gmail.com>
2022-10-15 11:39:17 -0400

    Adding enums for different scratch properties: scratchShape, resistance, and coat.

--- modules/scratch-model.mjs
+++ modules/scratch-model.mjs
@@ -1,4 +1,4 @@
-﻿export default function createItch(name = "", scratchShape = "", resistance = "", coat = "")
+﻿export default function createItch(name = "", scratchShape = scratchShape.star, resistance = resistance.medium, coat = coat.twoLayer)
 {
     return {
         "name" : name,
@@ -6,4 +6,23 @@
         "resistance" : resistance,
         "coat" : coat
     }
+}
+
+const scratchShape = {
+    square: 'square',
+    circle: 'circle',
+    rectangle: 'rectangle',
+    star: 'star'
+}
+
+const resistance = {
+    low: 'low',
+    medium: 'medium',
+    high: 'high'
+}
+
+const coat = {
+    oneLayer: 'one-layer',
+    twoLayer: 'two-layer',
+    threeLayer: 'three-layer'
 }
