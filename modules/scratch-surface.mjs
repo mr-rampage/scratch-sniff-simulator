dbdd81086012dba605e95f2aebeb32e3aea1d6e0
Yousef Sawires <sawires94@gmail.com>
2022-10-15 15:37:57 -0400

    touchmove action added

--- modules/scratch-surface.mjs
+++ modules/scratch-surface.mjs
@@ -27,6 +27,7 @@ function setupView(element) {
 function setupEvents(element) {
     element.addEventListener('mousemove', handleMouseMove(element), navigator.vibrate(200))
     element.addEventListener('touchmove', handleMouseMove(element), navigator.vibrate(200))
+    element.addEventListener('pointerdown', handleMouseMove(element), navigator.vibrate(200))
 }
 
 function createScratchUpEvent() {
